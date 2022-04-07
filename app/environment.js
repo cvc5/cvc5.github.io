301c7f77a13db7797fb7b54da851e1d0bd0d4db4
mudathirmahgoub <mudathirmahgoub@gmail.com>
2022-04-07 16:31:31 -0500

    changed logo

--- app/environment.js
+++ app/environment.js
@@ -5,7 +5,7 @@
 var __env = {}
 
 // production api Url. No need to change this when deploying the development branch
-// __env.apiUrl = "https://kind.cs.uiowa.edu/cvc5services/";
+__env.apiUrl = "https://kind.cs.uiowa.edu/cvc5services/";
 
 // Replace this file with env.dev.js to set development configuration
 
@@ -13,4 +13,4 @@ var __env = {}
 //__env.apiUrl = "https://kind.cs.uiowa.edu/cvcservices-dev/";
 
 // local development api Url
-__env.apiUrl = "http://localhost:8080/cvcservices_war/";
+// __env.apiUrl = "http://localhost:8080/cvcservices_war/";
